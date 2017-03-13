923cfdf02d0c43cfc6ae5cf542b8d234525a5520
joeseiden <joe.seiden214@gmail.com>
2017-03-13 11:25:25 -0700

    Add store

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
