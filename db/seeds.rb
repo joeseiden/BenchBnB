5174b28a82d7bf1ae4c5956c6c17370e6c03b6cd
joeseiden <joe.seiden214@gmail.com>
2017-03-13 17:00:49 -0700

    Add benches model, controller, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(
+  username: "guest",
+  password: "password"
+)
+
+Bench.create!(
+description: "lucasfilm, much star war",
+lat: 37.800224,
+lng: -122.448630
+)
+
+Bench.create!(
+description: "much app, many academy",
+lat: 37.791739,
+lng: -122.393111
+)
+
+Bench.create!(
+description: "these arts are so fine",
+lat: 37.801665,
+lng: -122.447788
+)
